6f2f3c5d5d2bc7766ad140d6f2954b2e1da258c8
Novantric <whyrl@DEEP-THOUGHT>
2022-11-05 23:01:26 +0000

    Database working??

--- Databases/DBBackend/Connection.cs
+++ Databases/DBBackend/Connection.cs
@@ -1,18 +1,27 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
+using System.Data.Common;
 using System.Data.SqlClient;
+using System.IO;
 
 namespace Brainstormer.Databases.DBBackend
 {
     internal class Connection
     {
         //Obtained from the example code and modified
-        private readonly string connStr = Properties.Settings.Default.DatabaseConnectionString;
+
+        //@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\whyrl\OneDrive - Anglia Ruskin University\Documents\Year2\Tri1\Software Engineering\myturn\Brainstormer\Databases\Database.mdf"";Integrated Security=True";
+        //Connstr works with local paths
+        private string connStr;
         private static Connection? _instance;
         private SqlConnection connectionDB;
 
         ///constructor
         private Connection()
         {
+            string fileName = @"Databases\Database.mdf";
+            string path = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, fileName);
+            connStr = string.Format(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""{0}"";Integrated Security = True", path);
             connectionDB = new SqlConnection(connStr);
         }        
 
